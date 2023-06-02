7c4a6943fd2211bb2bde18d44a3bc65b87d6d801
Pillong <jun00su00@naver.com>
2023-06-03 01:14:12 +0900

    Portal 스크립트 작성, 포탈 오브젝트와 Player 가 겹쳤을 때 위 방향키를 누르면 연결되어 있는 다른 포탈로 순간이동.

---
+++ Assets/Script/Portal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour 
+{
+	private bool isClicked = false;
+	private bool GetPortal = false;
+	private GameObject Player;
+	public GameObject Portal1;
+	private void OnTriggerEnter2D(Collider2D collision)//게임오브젝트 인자 받기
+	{
+		if (collision.CompareTag("Player"))
+		{
+			GetPortal = true;
+			Player = collision.gameObject;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)//포탈 사용 
+    {
+        if (isClicked && PlayerMove.instance.UseTeleport == false)
+        {
+            isClicked = false;
+            Player.transform.position = Portal1.transform.position;
+            PlayerMove.instance.UseTeleport = true;
+            StartCoroutine("PortalCoolTime");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GetPortal = false;
+    }
+    IEnumerator PortalCoolTime ()//포탈 사용 쿨타임 2초
+	{
+		yield return new WaitForSeconds(1.0f);
+		PlayerMove.instance.UseTeleport = false;
+	}
+    private void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.UpArrow) && GetPortal == true)
+        {
+
+			isClicked = true;
+        }
+    }
+}
