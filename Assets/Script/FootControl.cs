9dcce5683287f0ab90f2c5ebd1fcdfc75eb5cd0e
Pillong <jun00su00@naver.com>
2023-06-03 00:55:41 +0900

    플레이어 움직임 중 Jump 부분에서 버그 발생. FootControl C# 스크립트 작성. 플레이어의 발 부분에 충돌 처리하는 collider 생성 및 스크립트 적용 완료

---
+++ Assets/Script/FootControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootControl : MonoBehaviour 
+{
+    BoxCollider2D boxcollider;
+	void Start()
+	{
+        boxcollider = gameObject.GetComponent<BoxCollider2D>();
+	}
+	void Update()
+	{
+        if (PlayerMove.instance.rigid.velocity.y > 0)
+        {
+            boxcollider.isTrigger = true;
+        }
+        else
+        {
+            boxcollider.isTrigger = false;
+        }
+	}
+	/*void OnCollisionEnter2D(Collision2D other)
+    {
+        if (PlayerMove.instance.rigid.velocity.y == 0)
+        {
+            Debug.Log("111");
+            if (PlayerMove.instance.animator.GetCurrentAnimatorStateInfo(0).IsName("PlayerJump"))
+            {
+                Debug.Log("222");
+                if (other.gameObject.tag.Equals("Terrain"))
+                {
+                    Debug.Log("333");
+                    //other.gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+                    other.collider.isTrigger = false;
+                }
+            }
+        }
+        else if (PlayerMove.instance.rigid.velocity.y > 0)
+        {
+            Debug.Log("444");
+            if (PlayerMove.instance.animator.GetCurrentAnimatorStateInfo(0).IsName("PlayerJump"))
+            {
+                Debug.Log("555");
+                other.collider.isTrigger = true;
+            }
+        }
+    }*/
+	
+}
